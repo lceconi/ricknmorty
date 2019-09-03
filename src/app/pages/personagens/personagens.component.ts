0f1bd81beec9e312317d62f862b184eb0a1d1817
lceconi <laura.cec@hotmail.com>
2019-09-03 11:32:02 -0300

    Identação padronizada

--- src/app/pages/personagens/personagens.component.ts
+++ src/app/pages/personagens/personagens.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/services/api.service';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-personagens',
